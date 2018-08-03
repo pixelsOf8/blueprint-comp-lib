67c31461eb587c797e67f90ce4ce19e990f782a9
Graeme <graemenorman@tengroup.com>
2018-08-03 14:16:27 +0100

    Special Offer library removed from this repo - only single component per repo

--- libs/special_offer_lib/src/special_offer_lib.module.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DemoSpecialOfferLibModule } from './demo-special_offer-lib.module';
-
-describe('DemoSpecialOfferLibModule', () => {
-  it('should work', () => {
-    expect(new DemoSpecialOfferLibModule()).toBeDefined();
-  });
-});
