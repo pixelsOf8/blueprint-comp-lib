113560da802168d53c5ef1b74c9b2f5621c16a73
Graeme <graemenorman@tengroup.com>
2018-08-03 09:18:32 +0100

    Committing a demo simple header component with overridding theme file

---
+++ libs/header-lib/src/header-lib.module.spec.ts
@@ -0,0 +1,7 @@
+import { DemoHeaderLibModule } from './demo-header-lib.module';
+
+describe('DemoHeaderLibModule', () => {
+  it('should work', () => {
+    expect(new DemoHeaderLibModule()).toBeDefined();
+  });
+});
